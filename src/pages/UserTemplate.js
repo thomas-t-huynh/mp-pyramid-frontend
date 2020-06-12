e4efea3a7137b3b91a88676088a734160ba4245d
thomas <t_huynh927@yahoo.com>
2020-06-11 20:32:36 -0700

    finished selectCurrentWeek

--- src/pages/UserTemplate.js
+++ src/pages/UserTemplate.js
@@ -1,6 +1,13 @@
 import React from "react"
 import ProgramTemplate from "../components/ProgramTemplate"
 import moment from "moment"
+import { Link } from "react-router-dom"
+import styled from "styled-components"
+
+const StyledLink = styled(Link)`
+    color: black;
+    text-decoration: none;
+`
 
 function UserTemplate({template, setTemplate, setSelectedWorkout, setSelectedWeek}) {
     const handleSetSelectedWeek = () => {
@@ -11,16 +18,17 @@ function UserTemplate({template, setTemplate, setSelectedWorkout, setSelectedWee
         const currWeekStart = date.format('L')
         const currWeekStartIndex = template.findIndex(day => day.date === currWeekStart)
         if (currWeekStartIndex === -1) {
-            date.add(7, 'days')
-            console.log(date.format('L'))
+            // date.add(7, 'days')
+            return setSelectedWeek({start: 0, end: 7 })
         }
+        setSelectedWeek({ start: currWeekStartIndex, end: currWeekStartIndex + 7})
         
     }
     console.log(template)
     return (
         <div>
             <h1>User template</h1>
-            <button onClick={() => handleSetSelectedWeek()}>View Current week</button>
+            <StyledLink to="/template/week"><div onClick={() => handleSetSelectedWeek()}>View Current week</div></StyledLink>
             <ProgramTemplate template={template} setTemplate={setTemplate} setSelectedWorkout={setSelectedWorkout} setSelectedWeek={setSelectedWeek}/>
         </div>
     )
